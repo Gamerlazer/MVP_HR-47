f7f25a5a5645efa724f70becf23b53b1a8146fd9
Gamerlazer <gamerlazer@gmail.com>
2016-08-30 13:57:11 -0700

    Add favorites to favorite view

--- client/app/favorites/favorites.js
+++ client/app/favorites/favorites.js
@@ -1,7 +1,7 @@
 angular.module('favorites', [])
 
 .controller('favoritesController', function($scope, LogOut, serverRequests) {
-  $scope.favorites;
+  $scope.favorites = [];
   $scope.logout = function () {
     LogOut();
   };
@@ -12,10 +12,21 @@ angular.module('favorites', [])
         if (!favorites) {
           console.log('No favorites');
         } else {
-
+          favorites.data.forEach(function(biz) {
+            // console.log(biz);
+            var business = {
+              name: biz.bizName,
+              categories: biz.bizCategories.split(','),
+              displayAddress: biz.bizLocation.split(','),
+              ratingImgUrl: biz.bizRating,
+              bizImage: biz.bizImage
+            };
+            $scope.favorites.push(business);
+            // console.log(business, 'business');
+          })
           // var cats = favorites.data.bizCategories.split(',')
-          $scope.favorites = favorites.data;
-          console.log(favorites.data[0]);
+          // $scope.favorites = favorites.data;
+          // console.log(favorites.data[0]);
           // console.log(cats);
         }
       });
@@ -24,3 +35,27 @@ angular.module('favorites', [])
   $scope.getFavorites();
 
 });
+
+/*
+
+1
+bizCategories
+:
+"American (New),Brasseries"
+bizImage
+:
+"https://s3-media4.fl.yelpcdn.com/bphoto/uweSiOf0XBB4BPk_ibHVyg/ms.jpg"
+bizLocation
+:
+"500 Brannan St,SoMa,San Francisco, CA 94107"
+bizName
+:
+"Marlowe"
+bizRating
+:
+"https://s3-media4.fl.yelpcdn.com/assets/2/www/img/f62a5be2f902/ico/stars/v1/stars_small_4.png"
+createdAt
+:
+"2
+
+*/
